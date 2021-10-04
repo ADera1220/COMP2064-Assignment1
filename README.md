bf0a77f72d92c598192bc7f65d0f8af89c099ea8
ADera1220 <adera18@gmail.com>
2021-10-04 14:24:36 -0400

    Assignment 1 - Commit to ensure proper version control

---
+++ README.md
@@ -0,0 +1,7 @@
+<h1>Portfolio Website for Adam Dera</h1>
+<h2>Assignment 1</h2>
+
+<h5>Commit 1 | Oct 4, 2021 @ 2pm</h5>
+<p>This assignment's allowance to use outside frameworks inspired me to attempt a React.js site in conjunction with Node/Express.js. The idea of combining both was interesting so I looked it up and have provided a link to the walkthrough for connection below.</p>
+
+<a href='https://www.freecodecamp.org/news/create-a-react-frontend-a-node-express-backend-and-connect-them-together-c5798926047c/'>REACT/EXPRESS COMBINATION DONE WITH GUIDE AT FreeCodeCamp.com</a>
---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = process.env.PORT || 5000;
+
+//Route setup
+app.get('/', (req, res) => {
+    res.send('root route');
+})
+
+//Static file declaration
+app.use(express.static(path.join(__dirname, 'portfolio/build')));
+
+//Production mode
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'portfolio/build')));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname = 'portfolio/build/index.html'));
+    });
+}
+
+//Build mode
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/portfolio/public/index.html'));
+})
+
+//Start server
+app.listen(port, (req, res) => {
+    console.log(`server listening on port: ${port}`);
+})
