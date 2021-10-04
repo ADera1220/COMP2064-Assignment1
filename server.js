1cfd664dd246c33c87f75a3631da7efc060eac56
ADera1220 <adera18@gmail.com>
2021-10-04 15:17:40 -0400

    de-bugging application to allow for successful build on Heroku - changing directory paths to try and initialize the react App

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ if(process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, 'portfolio/build')));
 
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname + 'portfolio/build/index.html'));
+        res.sendFile(path.join(__dirname + '/portfolio/build/index.html'));
     });
 }
 
